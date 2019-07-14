c3ad08620e4b196c5406b9176ff696f03d1101ec
Alexandr Dudnik <dudnik2104@gmail.com>
2019-07-14 23:47:50 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.graphics.Rect
+import android.util.DisplayMetrics
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import kotlin.math.abs
@@ -22,6 +23,8 @@ fun Activity.isKeyboardClosed():Boolean{
 fun Activity.isKeyboardOpen():Boolean{
     val rect = Rect()
     this.window.decorView.getWindowVisibleDisplayFrame(rect)
+    val displayMetrics = DisplayMetrics()
+    windowManager.defaultDisplay.getMetrics(displayMetrics)
 
-    return abs(rect.height()-rect.bottom) > 128
+    return abs(displayMetrics.heightPixels-rect.height()) > 128
 }
